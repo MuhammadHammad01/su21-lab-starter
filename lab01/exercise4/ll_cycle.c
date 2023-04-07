170779fbcf2fc5f73b4b1117e84314d34265d964
Muhammad Hammad Bashir <hammadstudent17@gmail.com>
2023-04-07 23:39:45 +0500

    Updated ll_cycle.c

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,27 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    //First, checking head is NULL 
+    if (head==NULL)
+    {
+        return 0;
+    }
+    //initialize 
+    node *slow_ptr=head;
+    node *fast_ptr=head->next;
+    
+    //check whether NULL is met
+    while  ((fast_ptr!=NULL) && (fast_ptr->next!=NULL))
+    {
+        //if equal, then cyclic
+        if (fast_ptr==slow_ptr)
+        {
+            return 1;
+        }
+
+    fast_ptr=fast_ptr->next->next;
+    slow_ptr=slow_ptr->next;
+    }
+    //else acyclic
+    return 0;   
 }
